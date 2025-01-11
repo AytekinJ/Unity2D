4daeca58683b372714e90901986ec7c067f055e4
Aytekin <143596814+AytekinJ@users.noreply.github.com>
2025-01-11 16:49:44 +0300

    Update CameraFollow.cs

--- Player Codes/CameraFollow/CameraFollow.cs
+++ Player Codes/CameraFollow/CameraFollow.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 	public Transform target;
-	public Vector3 offset;
-	public float smooth;
+	public Vector3 offset = new Vector3(0, 0, -10);
+	public float smooth = 0.125f;
 
 	Vector3 velocity = Vector3.zero;
-	void FixedUpdate()
+	void FixedUpdate() //If player movement code runs in Update, this method should also run in Uptade
 	{
 		Vector3 movePosition = target.position + offset;
 		transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, smooth);
